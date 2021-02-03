02f524ffe886aaf84b1c3e58b54e8051dfa88fa3
Diego <dcastroaurora@gmail.com>
2021-02-03 09:47:08 -0500

    Avance de mi Portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarModule } from './@components/navbar/navbar.module';
+import { AboutModule } from './@components/about/about.module';
+import { ContactModule } from './@components/contact/contact.module';
+import { CopyrightModule } from './@components/copyright/copyright.module';
+import { FooterModule } from './@components/footer/footer.module';
+import { HeaderModule } from './@components/header/header.module';
+import { PortfolioModule } from './@components/portfolio/portfolio.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AboutModule,
+    ContactModule,
+    CopyrightModule,
+    FooterModule,
+    HeaderModule,
+    NavbarModule,
+    PortfolioModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
